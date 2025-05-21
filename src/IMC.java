2116b38f27f945a46ce403acadf7de1b4ab8705a
ALandvlz <alan_drn@hotmail.com>
2025-05-21 10:22:57 -0600

    Initial commit

---
+++ src/IMC.java
@@ -0,0 +1,14 @@
+public class IMC {
+
+    private int peso;
+    private int altura;
+
+    Public definicion(int peso, double altura){
+        this.peso = peso;
+        this.altura = altura;
+
+    }
+public int getPeso(){
+        return peso;
+}
+}
